ca77eaed9a3bb06ec7bd438bb7868ca95e93eb09
Samuel Vargas <savarga1@asu.edu>
2017-05-02 12:57:34 -0700

    rename imgui_wrapper to imgui_lua to more accurately reflect its purpose

---
+++ src/ui/imgui_lua.cc
@@ -0,0 +1,73 @@
+/*
+ * imgui_lua.cc
+ * Author: Samuel Vargas
+ * Date: 04/30/2017
+ */
+
+#include "imgui.h"
+#include "imgui_lua.h"
+#include "imgui_impl_sdl_gl3.h"
+#include "sol.h"
+#include <functional>
+#include <iostream>
+
+static const Video* video = nullptr;
+
+void ImGuiWrapper::bind(sol::state& lua, const Video& v) {
+   video = &v;
+   ImGui_ImplSdlGL3_Init(video->getWindowPtr());
+   lua.create_named_table("imgui");
+   auto imgui = lua["imgui"];
+   /// Core
+   imgui["NewFrame"]              = ImGuiWrapper::NewFrame;
+   imgui["Begin"]                 = ImGuiWrapper::Begin;
+   imgui["End"]                   = ImGuiWrapper::End;
+   imgui["Render"]                = ImGuiWrapper::Render;
+   /// Widgets
+   imgui["Text"]                  = ImGuiWrapper::Text;
+   imgui["Button"]                = ImGuiWrapper::Button;
+   /// Settings
+   imgui["EnableSoftwareMouse"]   = ImGuiWrapper::EnableSoftwareMouse;
+   /// Misc
+   imgui["ShowTestWindow"]        = ImGuiWrapper::ShowTestWindow;
+}
+
+/// Core
+void ImGuiWrapper::NewFrame() {
+   assert(video != nullptr && "Create an instance of ImGuiWrapper before calling this method");
+   ImGui_ImplSdlGL3_NewFrame(video->getWindowPtr());
+}
+
+bool ImGuiWrapper::Begin(std::string msg, bool collapse, int flags=0) {
+   ImGui::Begin(msg.c_str(), &collapse, flags);
+   return collapse;
+}
+
+void ImGuiWrapper::End() {
+   ImGui::End();
+}
+
+void ImGuiWrapper::Render() {
+   ImGui::Render();
+}
+
+
+/// Widgets
+void ImGuiWrapper::Text(std::string msg) {
+   ImGui::Text("%s", msg.c_str());
+}
+
+bool ImGuiWrapper::Button(std::string msg, size_t width, size_t height) {
+   return ImGui::Button(msg.c_str(), ImVec2(width, height));
+}
+
+/// Settings
+void ImGuiWrapper::EnableSoftwareMouse() {
+   ImGui::GetIO().MouseDrawCursor = true;
+}
+
+/// Misc
+bool ImGuiWrapper::ShowTestWindow(bool open) {
+   ImGui::ShowTestWindow(&open);
+   return open;
+}
---
+++ src/ui/imgui_lua.h
@@ -0,0 +1,24 @@
+/*
+ * imgui_lua.h
+ * Author: Samuel Vargas
+ */
+
+#pragma once
+#include "video.h"
+#include "sol.h"
+
+namespace ImGuiWrapper {
+   void bind(sol::state&, const Video&);
+   /// Core
+   static void NewFrame();
+   static bool Begin(std::string, bool, int);
+   static void End();
+   static void Render();
+   /// Widgets
+   static void Text(std::string);
+   static bool Button(std::string, size_t, size_t);
+   /// Settings
+   static void EnableSoftwareMouse();
+   /// Misc
+   static bool ShowTestWindow(bool);
+};
