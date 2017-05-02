8cebd19bcf76145d0a13c73d6e3c3a97b918cb12
Samuel Vargas <savarga1@asu.edu>
2017-05-02 13:04:06 -0700

    modify thisExePath to use SDL2 instead

--- src/util.cc
+++ src/util.cc
@@ -5,8 +5,12 @@
  */
 
 #include "util.h"
+#include <SDL2/SDL.h>
 
 /// Return absolute path of binary
 std::string thisExePath() {
+   char* p = SDL_GetBasePath();
+   std::string output = std::string(p);
+   free(p);
    return output;
 }
