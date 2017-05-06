dca65fdb92b74bcdd85777fce141cff03fc700a6
Samuel Vargas <savarga1@asu.edu>
2017-05-05 17:15:57 -0700

    introduce GLModel class

--- src/opengl/glmodel.cc
+++ src/opengl/glmodel.cc
@@ -3,5 +3,13 @@
  * Author: Samuel Vargas
  * Date: 05/03/2017
  *
- * A wrapper around tinyobj + 
+ * Models are wrappers around tinyobj + friends
+ * and represent a single object that can be rendered
+ * onscreen
  */
+
+#include "glmodel.h"
+
+GLModel::GLModel(std::string name) {
+   this->name = name;
+}
--- src/opengl/glmodel.h
+++ src/opengl/glmodel.h
@@ -3,9 +3,20 @@
  * Author: Samuel Vargas
  * Date: 05/03/2017
  *
- * A wrapper around tinyobj + 
+ * Models are wrappers around tinyobj + friends
+ * and represent a single object that can be rendered
+ * onscreen
  */
 
 #pragma once
+#include "tiny_obj_loader.h"
+#include "objwavefront.h"
+#include <iostream>
+
+#pragma once
 class GLModel {
+   private:
+      std::string name;
+   public:
+      GLModel(std::string);
 };
