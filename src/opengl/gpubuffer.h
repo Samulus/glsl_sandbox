40f866bc9615387a2ddf729a4e6f46cee91e34a4
Samuel Vargas <savarga1@asu.edu>
2017-05-05 17:26:19 -0700

    correct grammatical error in thrown gpubuffer exceptions

--- src/opengl/gpubuffer.h
+++ src/opengl/gpubuffer.h
@@ -47,23 +47,23 @@ namespace OpenGL {
 
    class InvalidAttributePosition : public std::range_error {
       public:
-         InvalidAttributePosition(unsigned char position, std::vector<GLfloat> data) : 
+         InvalidAttributePosition(unsigned char position, std::vector<GLfloat> data) :
             std::range_error(
-                  strFormat("Position (%d) is out of bounds, expected [0 .. %d]", 
+                  strFormat("Position (%d) is out of bounds, expected [0 .. %d]",
                      position, data.size() - 1)){}
    };
 
    class MismatchVertexCount : public std::runtime_error {
       public:
-         MismatchVertexCount(unsigned char position, 
-                             std::vector<GLVectorLen> vectorLen, 
-                             size_t numPoints, 
-                             std::vector<GLfloat> newVertexData) : 
+         MismatchVertexCount(unsigned char position,
+                             std::vector<GLVectorLen> vectorLen,
+                             size_t numPoints,
+                             std::vector<GLfloat> newVertexData) :
             std::runtime_error(
-                  strFormat("GPUBuffer has numPoints=%d and vectorLen[%d] = %d but"
-                               "but GPUBuffer::insert was called with %d elements"
-                               "instead (expected %d elements instead)", 
-                               numPoints, position, vectorLen[position], newVertexData.size(), 
+                  strFormat("GPUBuffer has numPoints=%d and vectorLen[%d] = %d but "
+                               "GPUBuffer::insert was called with %d elements"
+                               "instead (expected %d elements instead)",
+                               numPoints, position, vectorLen[position], newVertexData.size(),
                                vectorLen[position] * numPoints)) {}
       };
 }
