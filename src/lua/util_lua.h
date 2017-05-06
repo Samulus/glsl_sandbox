d97ef7fb2add64fb87d164ddbdedc0d08d84981d
Samuel Vargas <savarga1@asu.edu>
2017-05-05 17:22:51 -0700

    add util_lua.h module

---
+++ src/lua/util_lua.h
@@ -0,0 +1,28 @@
+/*
+ * util_lua.h
+ * Author: Samuel Vargas
+ * Date: 05/04/2017
+ */
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include "sol.h"
+
+template <typename T>
+std::vector<T> tableToVector(sol::table table) {
+   std::vector<T> output;
+   for (size_t i=1; i <= table.size(); ++i) {
+      output.push_back((T)table[i]);
+   }
+   return output;
+}
+
+template <typename T>
+sol::table arrayToTable(const T* array, size_t n) {
+   sol::table output;
+   for (size_t i=0; i < n; ++i) {
+      output[i+1] = array[i];
+   }
+   return output;
+}
