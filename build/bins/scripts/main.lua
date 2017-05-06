586943df32aaa04f4c76fde9ec7e329c60d1f311
Samuel Vargas <savarga1@asu.edu>
2017-05-05 17:27:53 -0700

    add fbo usage to main.lua

--- build/bins/scripts/main.lua
+++ build/bins/scripts/main.lua
@@ -1,8 +1,25 @@
+-- create fbo
+local fbo = gl.Framebuffer.new()
+
+--- setup MRT textures for deferred shading
+fbo:addTexture {
+   name     = 'position',
+   location = 0,
+   options  = {
+      gpuFormat   = gl.enum.RGB16F,
+      cpuFormat   = gl.enum.RGB,
+      cpuDataType = gl.enum.FLOAT,
+   },
+}
+
 function render()
+   fbo:bind()
+   fbo:unbind()
+
    imgui.NewFrame()
-   imgui.Begin("Hi", true)
+   imgui.Begin("Hi", true, 0)
    imgui.EnableSoftwareMouse()
-   imgui.ShowTestWindow()
+   imgui.ShowTestWindow(true)
    imgui.End()
    imgui.Render();
 end
