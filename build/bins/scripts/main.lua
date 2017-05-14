da06d5b155a295d03fb6aefe2bc581edbdadbcb4
Samuel Vargas <savarga1@asu.edu>
2017-05-13 17:54:40 -0700

    GLModel specific lua additions

--- build/bins/scripts/main.lua
+++ build/bins/scripts/main.lua
@@ -11,20 +11,25 @@ fbo:addTexture {
 }
 
 -- setup glsl
+-- Note: paths are relative to the BINARY, not this script
 local shader = gl.Shader.new {
                   vertex   = "./models/suzanne/suzanne.vs",
                   fragment = "./models/suzanne/suzanne.fs"
                }
 
 -- setup model
---[[
-local model = gl.Model.loadFromWavefront("suzanne.obj", "./models/suzanne")
-model:useVertexShader(vert);
-model:useFragmentShader(frag);
+local model = gl.Model.loadFromWavefront {
+                  filename = 'suzanne.obj',
+                  cat  = './models/suzanne'
+              }
+
+--("suzanne.obj", "./models/suzanne")
+--model:useVertexShader(vert);
+--model:useFragmentShader(frag);
 
 -- setup camera
-local camera = gl.Camera.new()
-camera:translate()
+--local camera = gl.Camera.new()
+--camera:translate()
 --]]
 
 function render()
@@ -33,9 +38,9 @@ function render()
    fbo:unbind()
 
    imgui.NewFrame()
-   imgui.Begin("Hi", true, 0)
+   --imgui.Begin("Hi", true, 0)
    imgui.EnableSoftwareMouse()
    imgui.ShowTestWindow(true)
-   imgui.End()
+   --imgui.End()
    imgui.Render();
 end
