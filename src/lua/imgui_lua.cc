59265c52bcce115b47ab47e39cee812bc16bfe46
Samuel Vargas <savarga1@asu.edu>
2017-05-29 23:15:05 +0000

    Replace boilerplate functions with lambdas (yay!)

--- src/lua/imgui_lua.cc
+++ src/lua/imgui_lua.cc
@@ -22,61 +22,56 @@ void ImGuiWrapper::bind(sol::state& lua, const Video& v) {
 
 
    /// Core
-   imgui["newFrame"]              = ImGuiWrapper::newFrame;
-   imgui["begin"]                 = ImGuiWrapper::begin;
-   imgui["end"]                   = ImGuiWrapper::end;
-   imgui["render"]                = ImGuiWrapper::render;
+   imgui["newFrame"]              = [] {
+                                       assert(video != nullptr &&
+                                             "Call ImGuiWrapper::bind() first");
+                                       ImGui_ImplSdlGL3_NewFrame(video->getWindowPtr());
+                                    };
+
+   imgui["begin"]                 = [] (std::string msg, bool collapse, int flags=0) {
+                                       auto result = ImGui::Begin(msg.c_str(), &collapse, flags);
+                                       return std::make_tuple(result, collapse);
+                                    };
+
+   imgui["end"]                   = &ImGui::End;
+   imgui["render"]                = &ImGui::Render;
+
+   /// Main Menus
+   imgui["beginMainMenuBar"]      = &ImGui::BeginMainMenuBar;
+   imgui["beginMenu"]             = [] (std::string label) {
+                                       return ImGui::BeginMenu(label.c_str(), true);
+                                    };
+
+   imgui["endMenu"]               = &ImGui::EndMenu;
+   imgui["endMainMenuBar"]        = &ImGui::EndMainMenuBar;
+
+   /// Menu Items
+   imgui["menuItem"]              = [] (std::string label, bool selected, bool enabled) {
+                                       return ImGui::MenuItem(label.c_str(), NULL, selected, enabled);
+                                    };
+
    /// Widgets
-   imgui["text"]                  = ImGuiWrapper::text;
-   imgui["button"]                = ImGuiWrapper::button;
+   imgui["text"]                  = [] (std::string text) {
+                                       ImGui::Text("%s", text.c_str());
+                                    };
+
+   imgui["button"]                = [] (std::string text, size_t w, size_t h) {
+                                       return ImGui::Button(text.c_str(), ImVec2(w, h));
+                                    };
    /// Settings
-   imgui["setFontSize"]           = ImGuiWrapper::setFontSize;
-   imgui["enableSoftwareMouse"]   = ImGuiWrapper::enableSoftwareMouse;
+   imgui["setFontSize"]           = [] (size_t fontSize) {
+                                       ImGuiIO& io = ImGui::GetIO();
+                                       io.Fonts->AddFontFromFileTTF((thisExePath() + "assets/roboto.ttf").c_str(),
+                                             fontSize);
+                                    };
+
+   imgui["enableSoftwareMouse"]   = [] {
+                                       ImGui::GetIO().MouseDrawCursor = true;
+                                    };
+
    /// Misc
-   imgui["showTestWindow"]        = ImGuiWrapper::showTestWindow;
-}
-
-/// Core
-void ImGuiWrapper::newFrame() {
-   assert(video != nullptr && "Create an instance of ImGuiWrapper before calling this method");
-   ImGui_ImplSdlGL3_NewFrame(video->getWindowPtr());
-}
-
-bool ImGuiWrapper::begin(std::string msg, bool collapse, int flags=0) {
-   ImGui::Begin(msg.c_str(), &collapse, flags);
-   return collapse;
-}
-
-void ImGuiWrapper::end() {
-   ImGui::End();
-}
-
-void ImGuiWrapper::render() {
-   ImGui::Render();
-}
-
-
-/// Widgets
-void ImGuiWrapper::text(std::string msg) {
-   ImGui::Text("%s", msg.c_str());
-}
-
-bool ImGuiWrapper::button(std::string msg, size_t width, size_t height) {
-   return ImGui::Button(msg.c_str(), ImVec2(width, height));
-}
-
-/// Settings
-void ImGuiWrapper::enableSoftwareMouse() {
-   ImGui::GetIO().MouseDrawCursor = true;
-}
-
-void ImGuiWrapper::setFontSize(size_t fontSize) {
-   ImGuiIO& io = ImGui::GetIO();
-   io.Fonts->AddFontFromFileTTF((thisExePath() + "assets/roboto.ttf").c_str(), fontSize);
-}
-
-/// Misc
-bool ImGuiWrapper::showTestWindow(bool open) {
-   ImGui::ShowTestWindow(&open);
-   return open;
+   imgui["showTestWindow"]        = [] (bool open) {
+                                       ImGui::ShowTestWindow(&open);
+                                       return open;
+                                    };
 }
--- src/lua/imgui_lua.h
+++ src/lua/imgui_lua.h
@@ -9,17 +9,4 @@
 
 namespace ImGuiWrapper {
    void bind(sol::state&, const Video&);
-   /// Core
-   static void newFrame();
-   static bool begin(std::string, bool, int);
-   static void end();
-   static void render();
-   /// Widgets
-   static void text(std::string);
-   static bool button(std::string, size_t, size_t);
-   /// Settings
-   static void setFontSize(size_t fontSize);
-   static void enableSoftwareMouse();
-   /// Misc
-   static bool showTestWindow(bool);
 };
