5d903bd9534ce2dba8a49411277f4e766032bd63
Samuel Vargas <savarga1@asu.edu>
2017-06-17 17:45:01 -0700

    Nothing important

--- build/bins/scripts/main.lua
+++ build/bins/scripts/main.lua
@@ -3,8 +3,8 @@ local      ui = require "scripts/ui"
 local   human = require "scripts/human"
 local suzanne = require "scripts/suzanne"
 
-imgui.setFontSize(30)
-sdl2.setWindowSize(30, 30, 3380, 2000)
+imgui.setFontSize(24)
+sdl2.setWindowSize(1000, 1000, 1280, 720)
 
 local proj = gml.perspective(120, 16/9, 0.1, 50)
 local view = gml.lookAt(
@@ -13,22 +13,19 @@ local view = gml.lookAt(
                  gml.vec3.new(0,1,0)
               )
 
-human.view   = view;
-suzanne.view = view;
-human.proj   = proj;
-suzanne.proj = proj;
-
+human.view   = view
+suzanne.view = view
+human.proj   = proj
+suzanne.proj = proj
 
 function render()
    video.setViewport()
-   video.clear(0.0,0.0,0.0)
+   video.clear(0.5,0.5,0.5)
 
    human.render()
    suzanne.render()
 
    imgui.newFrame()
-
-   --print(suzanne.proj)
 
    ui.matrixEdit ("Human", {
       { uniformName = 'proj',  tabName = 'Projection', matrix = human.proj},
